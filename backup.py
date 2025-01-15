ad1ea1588b6a8bcac08fdf8cfd137078440dec56
YourBoyRory <yourboyrory@gmail.com>
2025-01-15 00:11:47 -0500

    im so drunk rn idk what i did

--- backup.py
+++ backup.py
@@ -2,7 +2,7 @@ import zipfile
 import json
 import os
 import sys
-from PyQt5.QtWidgets import QMainWindow, QTextEdit, QApplication, QWidget, QPushButton, QVBoxLayout, QMenuBar, QMenu, QAction, QFileDialog, QLabel
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QTextEdit, QApplication, QWidget, QPushButton, QVBoxLayout, QMenuBar, QMenu, QAction, QFileDialog, QLabel
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -10,7 +10,7 @@ class MyWindow(QMainWindow):
 
         self.setWindowTitle('Backup WEBFISHING')
         self.setMinimumSize(400, 300)
-        
+
         # Create buttons
         self.pull_button = QPushButton('Download Save')
         self.push_button = QPushButton('Upload Save')
@@ -45,34 +45,59 @@ class MyWindow(QMainWindow):
         self.target_text_area = QTextEdit()
         self.target_text_area.setReadOnly(True)
         layout.addWidget(self.target_text_area)
-        
+
         self.load_config()
         self.game_path = self.config['save_folder_location']
         self.backup_path = self.config['backup_path']
 
     def pull_function(self):
+        self.notify("Pulling from Cloud...")
+
+        saveslot = "webfishing_save_slot_0.sav"
         game_path = self.config['save_folder_location']
-        backup_path = self.config['backup_path']
+        backup_path =  os.path.join(self.config['backup_path'],"webfishing_backup.zip")
+
         if backup_path == None:
             if not self.set_backup():
-                return 
-        
-        with zipfile.ZipFile(f"{backup_path}/webfishing_backup.zip", 'r') as zipf:
-            zipf.extractall(game_path)
-        print(f"Pull From: {backup_path} \nTo: {game_path}")
-        self.target_text_area.append(f"Pull From: {backup_path} \nTo: {game_path}\n")
+                self.notify("Pull Canceled.\n")
+                return
+        try:
+            if os.path.getmtime(os.path.join(game_path, saveslot)) >= os.path.getmtime(backup_path):
+                self.notify("Warning: Sync conflict. User intervention required.")
+                if not self.prompt("Remote files are not newer then Local.\nDo you still want to overwrite local?"):
+                    self.notify("Pull Canceled.\n")
+                    return
+            with zipfile.ZipFile(backup_path, 'r') as zipf:
+                zipf.extractall(game_path)
+            self.notify("Pull Successful!")
+        except:
+            self.notify("Pull Failed.")
+        self.notify(" ")
 
     def push_function(self):
-        game_path = self.config['save_folder_location']
-        backup_path = self.config['backup_path']
+        self.notify("Pushing to Cloud...")
+
+        saveslot = "webfishing_save_slot_0.sav"
+        save_path = os.path.join(self.config['save_folder_location'], saveslot)
+        backup_path =  os.path.join(self.config['backup_path'],"webfishing_backup.zip")
+
         if backup_path == None:
             if not self.set_backup():
-                return 
-        
-        with zipfile.ZipFile(f"{backup_path}/webfishing_backup.zip", 'w', zipfile.ZIP_DEFLATED) as zipf:
-            zipf.write(f"{game_path}/webfishing_migrated_data.save", arcname="webfishing_migrated_data.save")   
-        print(f"Push To: {backup_path} \nFrom: {game_path}")
-        self.target_text_area.append(f"Push To: {backup_path} \nFrom: {game_path}\n")
+                self.notify("Push Canceled.\n")
+                return
+        try:
+            if os.path.getmtime(save_path) <= os.path.getmtime(backup_path):
+                self.notify("Warning: Sync conflict. User intervention required.")
+                if not self.prompt("Local files are not newer then remote.\nDo you still want to overwrite remote?"):
+                    self.notify("Push Canceled.\n")
+                    return
+            with zipfile.ZipFile(backup_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                zipf.write(save_path, arcname=saveslot)
+            self.notify("Push Successful!")
+        except:
+            self.notify("Push Failed.")
+        self.notify(" ")
+
 
     def set_game(self):
         file_dialog = QFileDialog(self)
@@ -83,8 +108,7 @@ class MyWindow(QMainWindow):
             if selected_files:
                 game_path = selected_files[0]
                 self.config['save_folder_location'] = game_path
-                print(f"Selected Game Folder: {game_path}")
-                self.target_text_area.append(f"Selected Game Folder: {game_path}\n")
+                self.notify(f"Selected Game Folder: {game_path}")
                 self.save_config()
                 return True
         return False
@@ -98,8 +122,7 @@ class MyWindow(QMainWindow):
             if selected_folder:
                 backup_path = selected_folder[0]
                 self.config['backup_path'] = backup_path
-                print(f"Selected Backup Destination: {backup_path}")
-                self.target_text_area.append(f"Selected Backup Destination: {backup_path}\n")
+                self.notify(f"Selected Backup Destination: {backup_path}")
                 self.save_config()
                 return True
         return False
@@ -110,18 +133,24 @@ class MyWindow(QMainWindow):
                 self.config = json.load(file)
         except:
             self.config = {
-            #}
-            #    "backup_path": "./backup_folder",
                 "save_folder_location": "/home/rory/.local/share/Steam/steamapps/compatdata/3146520/pfx/drive_c/users/steamuser/AppData/Roaming/Godot/app_userdata/webfishing_2_newver"
             }
         if 'save_folder_location' not in self.config:
             self.set_game()
-        if 'backup_path' not in self.config:
-            self.set_backup()
-            
+
     def save_config(self):
         with open("config.json", "w") as file:
             json.dump(self.config, file)
+
+    def notify(self, string):
+        print(string)
+        self.target_text_area.append(string)
+
+    def prompt(self, promt):
+        reply = QMessageBox.question(self, 'Sync Conflict', promt, QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            return True
+        return False
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
